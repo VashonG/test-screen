bfd2fcdfc9803e6f564a35c9120b709633dd4d33
Vashon Gonzales <104737326+VashonG@users.noreply.github.com>
2023-01-14 20:00:38 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+var classes = {
+  "titan": {
+    "name": "Titan",
+    "attributes": "",
+    "description": "",
+    "ability": "",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  },
+  "hunter": {
+    "name": "Hunter",
+    "attributes": "Agility &bull; Adaptation &bull; Instinct",
+    "description": "Agile and daring, Hunters are quick on their feet and quicker on the draw.",
+    "ability": "Dodge: Avoid incoming enemy attacks with nimble skill.",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  },
+  "warlock": {
+    "name": "Warlock",
+    "attributes": "",
+    "description": "",
+    "ability": "",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  },
+  "adventurer": {
+    "name": "Adventurer",
+    "attributes": "",
+    "description": "",
+    "ability": "",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  },
+  "assassin": {
+    "name": "Assassin",
+    "attributes": "",
+    "description": "",
+    "ability": "",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  },
+  "monk": {
+    "name": "Monk",
+    "attributes": "",
+    "description": "",
+    "ability": "",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  },
+  "scout": {
+    "name": "Scout",
+    "attributes": "",
+    "description": "",
+    "ability": "",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  },
+  "rogue": {
+    "name": "Rogue",
+    "attributes": "",
+    "description": "",
+    "ability": "",
+    "icon": "",
+    "image": "",
+    "banner": ""
+  }
+}
